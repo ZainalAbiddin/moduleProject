38e222889186a7712ed8d08ceefe1c82886f550f
19971996 <salahuddinade@gmail.com>
2022-03-13 05:46:08 +0700

    Update modules.go

--- modules.go
+++ modules.go
@@ -59,14 +59,14 @@ func Ekstrakulikuler(kegiatan [4]string) {
 // interface
 
 // struct method
-func (n Nilaimurid) Nilai() {
-	fmt.Println(n.Namamurid)
-	fmt.Println("kelas :", n.kelas)
-	for _, nilai := range n.Rataannilai {
-		fmt.Println("===================")
-		fmt.Println(nilai.fisika)
-		fmt.Println(nilai.biologi)
-		fmt.Println(nilai.kimia)
-	}
+// func (n Nilaimurid) Nilai() {
+// 	fmt.Println(n.Namamurid)
+// 	fmt.Println("kelas :", n.kelas)
+// 	for _, nilai := range n.Rataannilai {
+// 		fmt.Println("===================")
+// 		fmt.Println(nilai.fisika)
+// 		fmt.Println(nilai.biologi)
+// 		fmt.Println(nilai.kimia)
+// 	}
 
-}
+// }
